8ad8fc3bb413a3f7b5697bafc01c1daf153931fd
mwakisaghu <harrisonchambela2016@gmail.com>
2022-02-04 13:16:43 +0300

    This adds Contacts and ContactItem components

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
 import uuid from 'uuid';
-import ContactContext from './contactContext';
+import ContactContext from './ContactContext';
 import contactReducer from './contactReducer';
 import {
   ADD_CONTACT,
