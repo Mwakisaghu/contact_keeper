93443e6e34812a60655e9357e66938c069635194
mwakisaghu <harrisonchambela2016@gmail.com>
2022-02-08 20:31:40 +0300

    This adds deleteContact functionality-to delete contacts from the UI

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -8,12 +8,20 @@ import {
   CLEAR_FILTER,
 } from '../types';
 
+/* eslint-disable */
 export default (state, action) => {
   switch (action.type) {
     case ADD_CONTACT:
       return {
         ...state,
         contacts: [...state.contacts, action.payload],
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
       };
     default:
       return state;
