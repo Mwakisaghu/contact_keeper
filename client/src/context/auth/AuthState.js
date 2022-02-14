fd6a9c189d8dc4a81741e34e8fff63576427bba8
mwakisaghu <harrisonchambela2016@gmail.com>
2022-02-14 12:29:15 +0300

    This shows Load User functionality & Set Authentication Token

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 
 import {
   REGISTER_SUCCESS,
@@ -27,6 +28,24 @@ const AuthState = (props) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   //Load user
+  const loadUser = async () => {
+    //load token into global headers
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    try {
+      const res = await axios.get('/api/auth');
+
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: AUTH_ERROR,
+      });
+    }
+  };
 
   //Register user
   const register = async (formData) => {
@@ -42,6 +61,8 @@ const AuthState = (props) => {
         type: 'REGISTER_SUCCESS',
         payload: res.data,
       });
+
+      loadUser();
     } catch (err) {
       dispatch({
         type: 'REGISTER_FAIL',
@@ -67,6 +88,7 @@ const AuthState = (props) => {
         error: state.error,
         register,
         clearErrors,
+        loadUser,
       }}
     >
       {props.children}
